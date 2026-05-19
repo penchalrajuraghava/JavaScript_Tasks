c038b2f839bfef26e6e65c9b3cc836fee759981d
penchalrajuraghava <penchalrajuraghava@gmail.com>
2026-05-19 08:17:30 +0530

    Added Task2 19-05-26

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JavaScript_Tasks
+
+# In this task yow will learn about
+
+# Java script data types and keywords let, var, and const
---
+++ Task_2/Task_2.js
@@ -0,0 +1,192 @@
+// Task : - 2
+
+// 1. Create a variable and store your favorite food.
+
+let favorite_food = "Chicken Biryani";
+
+// 2. Create a variable and store your mobile number.
+
+let mobile_number = 9858754518;
+
+// 3. Create a boolean variable with false value.
+
+let passed = false;
+
+// 4. Create one undefined variable and print typeof.
+
+let name;
+console.log(typeof name);
+
+// 5. Create one array with 6 animal names.
+let animals = ["tiger", "lion", "camel", "monkey", "crocodile", "wolf"];
+
+// 6. Print second animal from array.
+
+console.log(animals[1]);
+
+// 7. Print last animal from array using length.
+
+console.log(animals[animals.length - 1]);
+
+// 8. Create object with bike details (name, model, color).
+
+let bike = {
+  name: "Ns200",
+  model: "Beast",
+  color: "Orange",
+};
+
+// 9. Print bike color from object.
+
+console.log(bike.color);
+
+// 10. Create two variables and subtract values.
+let a = 10;
+let b = 20;
+
+console.log(a - b);
+
+// 11. Create two variables and divide values.
+
+let a1 = 20;
+let b1 = 5;
+console.log(a1 / b1);
+
+// 12. Find 15 % 2.
+
+console.log(15 % 2);
+
+// 13. Find 2 ** 5.
+
+console.log(2 ** 5);
+
+// 14. Create variable with 5 and use post increment.
+let number = 5;
+console.log(number++);
+console.log(number);
+
+// 15. Create variable with 10 and use pre increment.
+
+let number1 = 10;
+console.log(++number1);
+
+// 16. Create variable with 20 and use post decrement.
+let number2 = 20;
+console.log(number2--);
+console.log(number2);
+
+// 17. Create variable with 50 and use pre decrement.
+
+let number4 = 50;
+console.log(--number4);
+
+// 18. Check 100 > 50.
+
+console.log(100 > 50);
+
+// 19. Check 25 < 10.
+console.log(25 < 10);
+
+// 20. Check 50 >= 50.
+console.log(50 >= 50);
+
+// 21. Check 30 <= 25.
+console.log(30 <= 25);
+
+// 22. Check 10 == "10".
+console.log(10 == "10");
+
+// 23. Check 10 === "10".
+
+console.log(10 === "10");
+
+// 24. Check 25 != "25".
+
+console.log(25 != "25");
+
+// 25. Check 25 !== "25".
+console.log(25 !== "25");
+
+// 26. Use AND operator:
+//     10 > 5 && 20 > 15
+
+console.log(10 > 5 && 20 > 15);
+
+// 27. Use AND operator:
+//     5 > 10 && 20 > 15
+console.log(5 > 10 && 20 > 15);
+
+// 28. Use OR operator:
+//     5 > 10 || 20 > 15
+
+console.log(5 > 10 || 20 > 15);
+
+// 29. Use OR operator:
+//     2 > 5 || 1 > 10
+console.log(2 > 5 || 1 > 10);
+
+// 30. Use NOT operator:
+//     !(10 > 5)
+
+console.log(!(10 > 5));
+
+// 31. Use NOT operator:
+//     !(5 > 10)
+
+console.log(!(5 > 10));
+
+// 32. Create ternary operator:
+//     if 50 > 25 print "True"
+//     else print "False"
+
+let result = 20 > 25 ? "True" : "False";
+console.log(result);
+
+// 33. Create ternary operator:
+//     if 18 >= 21 print "Adult"
+//     else print "Minor"
+
+let age = 18 >= 21 ? "Adult" : "Minor";
+console.log(age);
+
+// 34. Create one array with 5 city names and print third city.
+
+let cities = [
+  "Hyderabad",
+  "bangalore",
+  "Kashmir",
+  "meghalaya",
+  "leh",
+  "HimachalPradesh",
+];
+
+// 35. Create one object with employee details and print employee name.
+
+console.log(cities[2]);
+
+// 36. Create variable with string "100" and print typeof.
+
+let number5 = "100";
+console.log(typeof number5);
+
+// 37. Create variable with number 100 and print typeof.
+
+let number6 = 100;
+console.log(typeof number6);
+
+// 38. Create one null variable and print it.
+let num = null;
+console.log(num);
+
+// 39. Create one array with mixed datatypes.
+
+let mix_type = ["penchal raju", 100, true, null, undefined];
+
+// 40. Create object with student details:
+//     name, mark, passed(boolean)
+
+let student_Details = {
+  name: "Penchal Raju",
+  Mark: "87",
+  passed: true,
+};
